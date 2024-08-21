c8e261b121296f3b6a51d4ac1827a26d46ef347b
benkeks <benjamin.bisping@tu-berlin.de>
2024-08-21 21:31:26 +0300

    Add filter for Isabelle source links

--- _extensions/benkeks/noindent/noindent.lua
+++ _extensions/benkeks/noindent/noindent.lua
@@ -23,3 +23,13 @@ function shy()
   end
 
 end
+
+
+function isb(args)
+  qualified_name = args[2] .. "." .. args[3]
+  return pandoc.Note {
+    pandoc.Str(tostring(args[1] .. " ")),
+    pandoc.Link(qualified_name,
+      "https://benkeks.github.io/ltbt-spectorscopy-isabelle/AFP/Lineartime_Branchingtime_Spectrum_I/" .. args[2] .. ".html#" .. qualified_name .. "|" .. args[1])
+  }
+end

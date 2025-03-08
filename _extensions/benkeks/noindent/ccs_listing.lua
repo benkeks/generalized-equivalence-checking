c1c3db8a3062873f39c9e2fb697cde12079e0ef5
benkeks <benjamin.bisping@tu-berlin.de>
2025-03-08 22:26:27 +0100

    Add own syntax for executable CCS examples

---
+++ _extensions/benkeks/noindent/ccs_listing.lua
@@ -0,0 +1,24 @@
+conf = {}
+
+function CodeBlock(block)
+  -- Check if a converter exists for this block. If not, return the block
+  -- unchanged.
+  local diagram_type = block.classes[1]
+  if "ccs" ~= block.classes[1] then
+    return nil
+  end
+
+  code_snippet = block.text
+  link_text = conf.link_text or "Example on equiv.io"
+  encoded_snippet = quarto.base64.encode(code_snippet)
+  local snippet_start = code_snippet:find("@snip")
+  if snippet_start then
+    block.text = code_snippet:sub(1, snippet_start - 1):gsub("%s+$", "")
+  end
+  return
+    pandoc.Blocks {
+      block,
+      pandoc.Link(link_text,
+        "https://equiv.io/#code=" .. encoded_snippet)
+    }
+end
